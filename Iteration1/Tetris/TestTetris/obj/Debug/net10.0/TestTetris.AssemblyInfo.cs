47feb4a876cd8176053ac3de1f5b5a217a76b3ea
AngelGitHub17 <kariaclub52@gmail.com>
2026-03-11 15:28:08 +0100

    just keybindings docs

--- Iteration1/Tetris/TestTetris/obj/Debug/net10.0/TestTetris.AssemblyInfo.cs
+++ Iteration1/Tetris/TestTetris/obj/Debug/net10.0/TestTetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestTetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd23d58370551d4c81ad5f40963f8074dae33a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestTetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestTetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
